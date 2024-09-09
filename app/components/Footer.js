5d333c027be300eec5ec4ecf98a7e421c988fbff
NeerajReddyK <118424327+NeerajReddyK@users.noreply.github.com>
2024-09-09 12:26:20 +0530

    Removed footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     <p
      className="text-center p-2 mt-4 mb-2 bg-purple-400"
     >
-      Thank you team Profile.fyi for this opportunity. Exicted to work for a startup! I hope that I meet your requirements.
     </p>
   )
 }
